8ddc72e85dcdc639a474857cc00fc04c6c13c8d3
Kostas <kachatzis@ece.auth.gr>
2021-07-08 01:19:30 +0300

    Add RST & PRST to the D-FF & JK-FF of ex.1

--- Exercise_1/src/d_ff.v
+++ Exercise_1/src/d_ff.v
@@ -1,22 +1,34 @@
-/*
-* D Flip-Flop
-* (Source: technobyte.org)
-*/
-`timescale 10ps/10ps;
-module d_ff (
-    input wire D, CLK, /*RST,*/
-    output reg Q, Qbar );
+/**
+ * Author:
+ *   Konstantinos Chatzis
+ *   June 2021
+ *   kachatzis <at> ece.auth.gr
+ */
 
-    always @(posedge CLK /*or posedge RST*/) 
+/**
+ * D-FF
+ * Positive edge triggered.
+ * RST, PRST: Active-HIGH.
+ * 
+ * RST:  Resets the D-FF to Q=0.
+ * PRST: Sets the D-FF to the preset Q=1.
+ */
+module d_ff (
+    output reg Q, Qn,
+    input wire D, CLK, RST, PRST
+);
+
+    assign Qn = ~Q;
+    
+    always @(posedge CLK or posedge RST or posedge PRST) 
     begin
-        // if( RST == 1 ) begin
-        //     #1; // Delay
-        //     Q <= 0;
-        //     Qbar <= 1;
-        // end else begin 
-        Q <= D; 
-        Qbar <= ~D; 
-        // end
+        if( RST ) begin
+            Q  <= 0;
+        end else if ( PRST ) begin 
+            Q  <= 1;
+        end else begin
+            Q  <= D; 
+        end
     end 
 
 endmodule
--- Exercise_1/src/jk_ff.v
+++ Exercise_1/src/jk_ff.v
@@ -1,20 +1,39 @@
-`timescale 10ps/10ps;
+/**
+ * Author:
+ *   Konstantinos Chatzis
+ *   June 2021
+ *   kachatzis <at> ece.auth.gr
+ */
+
+/**
+ * JK-FF
+ * Positive edge triggered.
+ * RST, PRST: Active-HIGH.
+ * 
+ * RST:  Resets the JK-FF to Q=0.
+ * PRST: Sets the JK-FF to the preset Q=1.
+ */
 module jk_ff (
     input wire J, K, CLK,
-    output reg Q, Qbar );
+    output reg Q, Qn, RST, PRST );
 
-    always @(negedge CLK /*or posedge RST*/) 
+    assign Qn = ~Q;
+
+    always @(posedge CLK or posedge RST or posedge PRST) 
     begin
-        if ( J && K ) begin
+        if (RST) begin
+            Q <= 0;
+        end else if (PRST) begin
+            Q <= 1;
+        end else if ( J & K ) begin
             Q <= ~Q; 
-            Qbar <= Q;
         end else if ( J ) begin
-            Q = 1;
-            Qbar = 0;
+            Q <= 1;
         end else if ( K ) begin
-            Q = 0;
-            Qbar = 1;
-        end 
+            Q <= 0;
+        end else begin
+            Q <= 0;
+        end
     end 
 
 endmodule
