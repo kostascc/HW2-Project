575924525431876e7af2e8036c7c2741e6afc259
Kostas <kachatzis@ece.auth.gr>
2021-07-09 01:45:44 +0300

    Add Hamming encoder

---
+++ Exercise_3/src/hamEncode125.v
@@ -0,0 +1,51 @@
+/**
+ * Author:
+ *   Konstantinos Chatzis
+ *   June 2021
+ *   kachatzis <at> ece.auth.gr
+ */
+ `timescale 1ns/1ns
+ module hamEncode125(
+    output reg[17:1]  OUT,
+    input  wire[11:0]  IN
+);
+
+    reg[4:0] PAR;
+    assign OUT = {
+        IN[11], //17
+        PAR[4],   //16
+        IN[10], //15
+        IN[9],  //14
+        IN[8],  //13
+        IN[7],  //12
+        IN[6],  //11
+        IN[5],  //10
+        IN[4],  //9
+        PAR[3],   //8
+        IN[3],  //7
+        IN[2],  //6
+        IN[1],  //5
+        PAR[2],   //4
+        IN[0],  //3
+        PAR[1],   //2
+        PAR[0]    //1
+    };
+
+
+    assign PAR[0]  = OUT[3]  ^ OUT[5]  ^ 
+                     OUT[7]  ^ OUT[9]  ^ OUT[11] ^ 
+                     OUT[13] ^ OUT[15] ^ OUT[17] ;
+
+    assign PAR[1]  = OUT[3]  ^
+                     OUT[6]  ^ OUT[7]  ^
+                     OUT[10] ^ OUT[11] ^
+                     OUT[14] ^ OUT[15];
+
+    assign PAR[2]  = OUT[5]  ^ OUT[6]  ^ OUT[7]  ^
+                     OUT[12] ^ OUT[13] ^ OUT[14] ^ OUT[15] ;
+
+    assign PAR[3]  = OUT[9]  ^ OUT[10] ^ OUT[11] ^ OUT[12] ;
+
+    assign PAR[4] = OUT[17] ;
+
+endmodule
---
+++ Exercise_3/src/hamEncode125_TB.v
@@ -0,0 +1,43 @@
+/**
+ * Author:
+ *   Konstantinos Chatzis
+ *   June 2021
+ *   kachatzis <at> ece.auth.gr
+ */
+ `timescale 10ns/1ns
+module hamEncode125_TB;
+
+    wire[16:0] OUT;
+    reg[11:0]  IN;
+    reg clk;
+
+    integer i;
+    reg[28:0] testVector[23:0];
+    reg[16:0] expectedOUT;
+
+    hamEncode125 dut(
+        .IN(IN),
+        .OUT(OUT)
+    );
+
+    initial begin
+        clk = 0;
+        i = 0;
+        expectedOUT = {17{1'b0}};
+        $readmemb("hamEncoder125_TB_Vector", testVector);
+        for(i=0; i<24; i=i+1) begin
+            #10;
+            {IN,expectedOUT} = testVector[i];
+        end
+    end
+
+    always @(negedge clk) begin
+        if ({OUT[11:0]} != {expectedOUT[11:0]})
+                $display("Wrong Output at i=%d!{%b, %b}", {i}, {OUT[11:0]}, {expectedOUT[11:0]});
+    end
+
+    always begin
+        #5 clk = ~clk;
+    end
+
+endmodule
